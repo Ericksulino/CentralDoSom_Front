95a9c01098b5e38df071c6861b259bee9f78206e
Ericksulino <ericksulino@gmail.com>
2023-02-20 09:56:40 -0300

    inplementando pesquisa

--- src/controllers/front.controller.js
+++ src/controllers/front.controller.js
@@ -1,10 +1,18 @@
+const { search } = require("../../../API_CentalDoSom/src/routes/user.route");
 
 const getHome = (req,res) =>{
    // Recuperar informações da URL
     const {filter} = req.query;
+    const {seach} = req.query;
+    var flag = true;
     let API_URL = 'http://localhost:5000/item?limit=1000';
     if(filter){
       API_URL = 'http://localhost:5000/item/category?categoria='+filter; 
+      flag = false;
+    }
+    else if(seach){
+      flag = false;
+      API_URL = 'http://localhost:5000/item/seach?nome='+seach;
     }
 
     async function fetchData() {
@@ -19,7 +27,7 @@ const getHome = (req,res) =>{
     
         const data = await response.json();
         //console.log(data.produtos);
-        res.render("home",{produtos: data.produtos});
+        res.render("home",{meio: flag,produtos: data.produtos});
       } catch (error) {
         console.error(error);
         res.render("home");
