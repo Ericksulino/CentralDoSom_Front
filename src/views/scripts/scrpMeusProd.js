535330f26616f5faea5171a5e691b2c3b5f9f4df
Ericksulino <ericksulino@gmail.com>
2023-02-24 14:23:10 -0300

    melhorando e protegendo a área página meus produtos

--- src/views/scripts/scrpMeusProd.js
+++ src/views/scripts/scrpMeusProd.js
@@ -1,54 +1,78 @@
-//var token = localStorage.getItem("token");
-fetch('https://api-centraldosom.onrender.com/item/byUser',{
-  method: 'GET',
-  headers: {
-    'Authorization': 'Bearer ' + token
-  },
-}).then(response => response.json())
-  .then(data => {
-    // seleciona o contêiner na página onde os itens serão exibidos
-    const container = document.getElementById('itens');
-    
-    // itera sobre os dados e adiciona o conteúdo gerado para cada item
-    data.produtos.forEach(item => {
-      // gera o conteúdo HTML usando o código acima e os dados do item
-      const itemHtml  = `
-        <div class="item">
-          <a href="/More?imagem=${item.foto}&nome=${item.nome}&valor=${item.valor}&categoria=${item.categoria}&tipo=${item.tipo}&descricao=${item.descricao}&anunciante=${item.userName}&telefone=${item.userNumber}&cidade=${item.userCity}&UF=${item.userUF}">
-            <img src="https://api-centraldosom.onrender.com/imagens/${item.foto}" alt="">
-          </a>
-          <p>${item.nome}</p>
-          <h5>R$${item.valor}</h5>
-          <h3>${item.tipo}</h3>
-          <a href="CadItem?imagem=${item.foto}&nome=${item.nome}&valor=${item.valor}&categoria=${item.categoria}&tipo=${item.tipo}&descricao=${item.descricao}&anunciante=${item.userName}&telefone=${item.userNumber}&cidade=${item.userCity}&UF=${item.userUF}">
-          <button type="button" class="button green" id="edit-${item.nome}">editar</button>
-          <a/>
-          <a href="/">
-          <button type="button" class="button red" id="delete-${item.nome}">excluir</button>
-          <a/>
-          <div class="modal" id="modal">
-          <div class="modal-content">
-            <span class="close-button" onclick="closeModal()">
-              &times;
-            </span>
-          
-            <h1 id="modal-message"></h1>
-            <button type="button" class="button sim" id="edit-${item.nome}">Sim</button>
-            <button type="button" class="button nao" id="delete-${item.nome}">Não</button>
-          
-          </div>
-        </div>
-        </div>
-      `;
-      // adiciona o conteúdo gerado ao contêiner na página
-      container.insertAdjacentHTML('beforeend', itemHtml);
-    });
-  })
-  .catch(error => {
-    console.error('Ocorreu um erro:', error);
-  });
-
+const welcome = document.getElementById('welcome');
+if (!token) {
+   window.location.href = '/login';
+}
+  fetch('https://api-centraldosom.onrender.com/auth', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + token
+    },
+  }).then(res => {
+        if (res.status === 201) {
+          res.json().then(data => {
+            console.log(data.message);
+            welcome.innerText = "Bem vindo(a) " + data.user;
+          })
+              //var token = localStorage.getItem("token");
+              fetch('https://api-centraldosom.onrender.com/item/byUser',{
+                method: 'GET',
+                headers: {
+                  'Authorization': 'Bearer ' + token
+                },
+              }).then(response => response.json())
+                .then(data => {
+                  // seleciona o contêiner na página onde os itens serão exibidos
+                  const container = document.getElementById('itens');
+                  
+                  // itera sobre os dados e adiciona o conteúdo gerado para cada item
+                  data.produtos.forEach(item => {
+                    // gera o conteúdo HTML usando o código acima e os dados do item
+                    const itemHtml  = `
+                      <div class="item">
+                        <a href="/More?imagem=${item.foto}&nome=${item.nome}&valor=${item.valor}&categoria=${item.categoria}&tipo=${item.tipo}&descricao=${item.descricao}&anunciante=${item.userName}&telefone=${item.userNumber}&cidade=${item.userCity}&UF=${item.userUF}">
+                          <img src="https://api-centraldosom.onrender.com/imagens/${item.foto}" alt="">
+                        </a>
+                        <p>${item.nome}</p>
+                        <h5>R$${item.valor}</h5>
+                        <h3>${item.tipo}</h3>
+                        <a href="CadItem?imagem=${item.foto}&nome=${item.nome}&valor=${item.valor}&categoria=${item.categoria}&tipo=${item.tipo}&descricao=${item.descricao}&anunciante=${item.userName}&telefone=${item.userNumber}&cidade=${item.userCity}&UF=${item.userUF}">
+                        <button type="button" class="button green" id="edit-${item.nome}">editar</button>
+                        <a/>
+                        <a href="/">
+                        <button type="button" class="button red" id="delete-${item.nome}">excluir</button>
+                        <a/>
+                        <div class="modal" id="modal">
+                        <div class="modal-content">
+                          <span class="close-button" onclick="closeModal()">
+                            &times;
+                          </span>
+                        
+                          <h1 id="modal-message"></h1>
+                          <button type="button" class="button sim" id="edit-${item.nome}">Sim</button>
+                          <button type="button" class="button nao" id="delete-${item.nome}">Não</button>
+                        
+                        </div>
+                      </div>
+                      </div>
+                    `;
+                    // adiciona o conteúdo gerado ao contêiner na página
+                    container.insertAdjacentHTML('beforeend', itemHtml);
+                  });
+                })
+                .catch(error => {
+                  console.error('Ocorreu um erro:', error);
+                });
   
+        } else if (res.status === 400 || 500) {
+          res.json().then(data => {
+            console.log(data.message);
+          })
+          window.location.href = '/login';
+         
+        }
+       
+      });
+
     function showModal(message, action,modal,css) {
       currentAction = action;
       var element = document.getElementById(modal);
