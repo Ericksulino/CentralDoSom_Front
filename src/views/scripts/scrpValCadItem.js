a832319001bf5ca6c9f981649d51c6ba27a4fa37
Ericksulino <ericksulino@gmail.com>
2023-02-24 14:32:06 -0300

    protegendo a página de caditem

--- src/views/scripts/scrpValCadItem.js
+++ src/views/scripts/scrpValCadItem.js
@@ -4,57 +4,78 @@ const form = document.getElementById('form');
 const campos = document.querySelectorAll('.required');
 const spans = document.querySelectorAll('.span-required');
 const image = document.getElementById('myFile');
+if (!token) {
+  window.location.href = '/login';
+}
+ fetch('https://api-centraldosom.onrender.com/auth', {
+   method: 'GET',
+   headers: {
+     'Authorization': 'Bearer ' + token
+   },
+ }).then(res => {
+       if (res.status === 201) {
+         res.json().then(data => {
+           console.log(data.message);
+         })
+            form.addEventListener('submit',(event)=>{
+              event.preventDefault();
+              nameValidate();
+              cateValidade();
+              condValidade();
+              descValidate();
+              valValidate();
+              imgValidate();
 
-form.addEventListener('submit',(event)=>{
-  event.preventDefault();
-  nameValidate();
-  cateValidade();
-  condValidade();
-  descValidate();
-  valValidate();
-  imgValidate();
+              let isValid = true;
+              for(let i = 0; i < campos.length; i++){
+                if(campos[i].style.border == '2px solid rgb(230, 54, 54)'){
+                  isValid = false;
+                  break;
+                }
+              }
+            console.log(isValid)
+              if(isValid){
+              var token = localStorage.getItem("token");
+              const formData = new FormData();
+              formData.append('nome', campos[0].value);
+              formData.append('categoria', campos[1].value);
+              formData.append('tipo', campos[2].value);
+              formData.append('descricao', campos[3].value);
+              formData.append('valor', campos[4].value);
+              formData.append('file', image.files[0]);
+              fetch('https://api-centraldosom.onrender.com/item', {
+                  method: 'POST',
+                  headers: {
+                    'Authorization': 'Bearer ' + token
+                  },
+                  body: formData
+                }).then(res => {
+                      if (res.status === 201) {
+                        res.json().then(data => {
+                          console.log(data.message);
+                        })
+                        window.location.href = '/produtos';
+                
+                      } else if (res.status === 400 || 500) {
+                        res.json().then(data => {
+                          console.log(data.message);
+                        })
+                        spans[6].style.display = 'block';
+                        //window.location.href = '/CadUser';
+                      }
+                    
+                    })
+                
+              } 
+            });
+} else if (res.status === 400 || 500) {
+  res.json().then(data => {
+    console.log(data.message);
+  })
+  window.location.href = '/login';
+ 
+}
 
-  let isValid = true;
-  for(let i = 0; i < campos.length; i++){
-    if(campos[i].style.border == '2px solid rgb(230, 54, 54)'){
-      isValid = false;
-      break;
-    }
-  }
-console.log(isValid)
-  if(isValid){
-  var token = localStorage.getItem("token");
-  const formData = new FormData();
-  formData.append('nome', campos[0].value);
-  formData.append('categoria', campos[1].value);
-  formData.append('tipo', campos[2].value);
-  formData.append('descricao', campos[3].value);
-  formData.append('valor', campos[4].value);
-  formData.append('file', image.files[0]);
-  fetch('https://api-centraldosom.onrender.com/item', {
-      method: 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      body: formData
-    }).then(res => {
-          if (res.status === 201) {
-            res.json().then(data => {
-              console.log(data.message);
-            })
-            window.location.href = '/produtos';
-    
-          } else if (res.status === 400 || 500) {
-            res.json().then(data => {
-              console.log(data.message);
-            })
-            spans[6].style.display = 'block';
-            //window.location.href = '/CadUser';
-          }
-         
-        })
-    
-  } 
 });
 
 
